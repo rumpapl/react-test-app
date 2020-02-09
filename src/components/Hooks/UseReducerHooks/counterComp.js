068a8441f2e6213c955cb8754b17f284312c57cf
rumpapl <rumpakucse14@gmail.com>
2020-02-10 02:14:39 +0600

    worked on useReducer hooks (complex state and action)

--- src/components/Hooks/UseReducerHooks/counterComp.js
+++ src/components/Hooks/UseReducerHooks/counterComp.js
@@ -1,13 +1,20 @@
 import React, { useReducer } from 'react';
 import { Button } from 'react-bootstrap';
 
-const initialState = 0;
+const initialState = {
+    counterOne: 0,
+    counterTwo: 20,
+};
 const reducerFunction = (currentState, action) => {
     switch (action.type) {
-        case '+':
-            return currentState + action.value;
-        case '-':
-            return currentState - action.value;
+        case 'Counter One +':
+            return { ...currentState, counterOne: currentState.counterOne + action.value };
+        case 'Counter One -':
+            return { ...currentState, counterOne: currentState.counterOne - action.value };
+        case 'Counter Two +':
+            return { ...currentState, counterTwo: currentState.counterTwo + action.value };
+        case 'Counter Two -':
+            return { ...currentState, counterTwo: currentState.counterTwo - action.value };
         case 'reset':
             return initialState;
         default:
@@ -21,16 +28,27 @@ const counterComp = () => {
 
     return (
         <div>
-            <h2>Count: {newState}</h2>
-            <Button onClick={() => dispatch({ type: '+', value: 1 })}>+</Button>
+            <h2>Counter 1: {newState.counterOne}</h2>
+            <Button onClick={() => dispatch({ type: 'Counter One +', value: 1 })}>+</Button>
             &ensp;
-            <Button onClick={() => dispatch({ type: '-', value: 1 })}>-</Button>
+            <Button onClick={() => dispatch({ type: 'Counter One -', value: 1 })}>-</Button>
             &ensp;
-            <Button onClick={() => dispatch({ type: '+', value: 5 })}>+5</Button>
+            <Button onClick={() => dispatch({ type: 'Counter One +', value: 5 })}>+5</Button>
             &ensp;
-            <Button onClick={() => dispatch({ type: '-', value: 5 })}>-5</Button>
+            <Button onClick={() => dispatch({ type: 'Counter One -', value: 5 })}>-5</Button>
+
+            <h2>Counter 2: {newState.counterTwo}</h2>
+            <Button onClick={() => dispatch({ type: 'Counter Two +', value: 1 })}>+</Button>
             &ensp;
-            <Button onClick={() => dispatch({ type: "reset" })}>0</Button>
+            <Button onClick={() => dispatch({ type: 'Counter Two -', value: 1 })}>-</Button>
+            &ensp;
+            <Button onClick={() => dispatch({ type: 'Counter Two +', value: 5 })}>+5</Button>
+            &ensp;
+            <Button onClick={() => dispatch({ type: 'Counter Two -', value: 5 })}>-5</Button>
+            <br />
+            <br />
+            <Button onClick={() => dispatch({ type: "reset" })}>Reset Both Counter</Button>
+
         </div>
     );
 };
